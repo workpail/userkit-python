0a8a6f8f615f58e150cb9adc59363a38f39ae8b9
David Toth <david@workpail.com>
2016-11-03 18:15:58 -0400

    Rename test-method

--- userkit/test/test_emails.py
+++ userkit/test/test_emails.py
@@ -2,11 +2,11 @@ from base_test import BaseTestCase
 
 
 class TestEmails(BaseTestCase):
-    """We can only test wrong email_keys since a real key can only
+    """We can only test bad email_keys since a real key can only
     be obtained by UserKit POSTing to a developer's email_webhook
     endpoint.
     """
 
-    def test_get_invite_bad_token(self):
+    def test_get_pending_email_bad_key(self):
         email = self.uk.emails.get_pending_email('completelywrong-key-here')
         self.assertIsNone(email)
